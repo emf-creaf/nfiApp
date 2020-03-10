9e15ba81989b73ee7167aab57589602fb5def32f
MalditoBarbudo <victorgrandagarcia@gmail.com>
2020-03-10 16:43:41 +0100

    logic for variables available and filter var selectors added

--- R/helpers.R
+++ R/helpers.R
@@ -34,3 +34,113 @@ text_translate <- function(text, lang, texts_thes) {
       ~ text_df[text_df$text_id == .x, glue::glue('text_{lang}')]
     )
 }
+
+# main table (results) to look at
+main_table_to_look_at <- function(nfi, desglossament, diameter_classes) {
+  # the logic is as follows:
+  #   - except for shrubs and regen, the name of the main table is built by
+  #     the desgossament level, the nfi and the diameter_classes if checked
+  #   - a simple switch must work
+  if (isTRUE(diameter_classes)) {
+    dc <- 'DIAMCLASS_'
+  } else {
+    dc <- ''
+  }
+
+  main_table <- switch(
+    nfi,
+    'nfi_2' = glue::glue('{desglossament}_nfi_2_{dc}results'),
+    'nfi_3' = glue::glue('{desglossament}_nfi_3_{dc}results'),
+    'nfi_4' = glue::glue('{desglossament}_nfi_4_{dc}results'),
+    'nfi_2_nfi_3' = glue::glue('{desglossament}_comp_nfi2_nfi3_{dc}results'),
+    'nfi_3_nfi_4' = glue::glue('{desglossament}_comp_nfi3_nfi4_{dc}results'),
+    'nfi_2_shrub' = glue::glue('shrub_nfi_2_info'),
+    'nfi_3_shrub' = glue::glue('shrub_nfi_3_info'),
+    'nfi_4_shrub' = glue::glue('shrub_nfi_4_info'),
+    'nfi_2_regen' = glue::glue('regeneration_nfi_2'),
+    'nfi_3_regen' = glue::glue('regeneration_nfi_3'),
+    'nfi_4_regen' = glue::glue('regeneration_nfi_4')
+  )
+
+  return(main_table)
+}
+
+# ancillary tables to look at
+ancillary_tables_to_look_at <- function(nfi) {
+  # the logic is as follows:
+  #   - plots is always an ancillary plot
+  #   - except for the comparision data, the nfi_X indicates the dyanmic table
+  #     to retrieve
+  if (nfi %in% c('nfi_2_nfi_3', 'nfi_3_nfi_4')) {
+    ancillary_tables <- 'plots'
+  } else {
+    nfi_strip <- stringr::str_extract(nfi, 'nfi_[2-4]')
+    ancillary_tables <- c(
+      'plots', glue::glue({"plots_{nfi_strip}_dynamic_info"})
+    )
+  }
+  return(ancillary_tables)
+}
+
+# translate variables function. Similar as text_translate, but for variables
+translate_var <- function(
+  vars, tables, lang,
+  var_thes, numerical_thes,
+  is_summary = FALSE, need_order = TRUE
+) {
+  if (isTRUE(is_summary)) {
+
+  } else {
+    var_lookup_table <-
+      var_thes %>%
+      dplyr::select(
+        dplyr::one_of(
+          'var_id', glue::glue('translation_{lang}'),
+          'var_order_app', 'var_table'
+        )
+      ) %>%
+      dplyr::filter(var_id %in% vars, var_table %in% tables) %>%
+      dplyr::left_join(
+        numerical_thes %>%
+          dplyr::select(var_id, var_table, var_units),
+        by = c('var_id', 'var_table')
+      ) %>%
+      dplyr::mutate(
+        var_units = dplyr::if_else(
+          is.na(var_units), NA_character_,
+          as.character(glue::glue("[{var_units}]"))
+        )
+      ) %>%
+      tidyr::unite(
+        col = var_name,
+        !!rlang::sym(glue::glue("translation_{lang}")), var_units,
+        sep = ' '
+      ) %>%
+      dplyr::mutate(var_name = stringr::str_remove(var_name, ' NA')) %>%
+      dplyr::select(-dplyr::one_of(
+        'var_table'
+      )) %>%
+      dplyr::distinct() %>%
+      as.data.frame()
+  }
+
+  if (isTRUE(need_order)) {
+    order_of_vars <-
+      var_lookup_table %>%
+      dplyr::arrange(var_order_app) %>%
+      dplyr::pull(var_id) %>%
+      match(vars, .) %>%
+      order()
+
+    ordered_res <- vars[order_of_vars] %>%
+      magrittr::set_names(var_lookup_table[order_of_vars, 'var_name'])
+
+    return(ordered_res)
+  } else {
+    var_lookup_table %>%
+      dplyr::pull(var_id) %>%
+      magrittr::set_names(
+        var_lookup_table %>% dplyr::pull(var_name)
+      )
+  }
+}
--- R/mod_filtersUI.R
+++ R/mod_filtersUI.R
@@ -12,6 +12,7 @@ mod_filtersUI <- function(id) {
 
   # UI ####
   shiny::tagList(
+    shiny::br(),
     shiny::uiOutput(
       ns('mod_filters_container')
     )
@@ -39,10 +40,60 @@ mod_filters <- function(
   data_reactives,
   var_thes, numerical_thes, texts_thes
 ) {
+
+  # variables available ####
+  variables_available <- shiny::reactive({
+    nfi <- data_reactives$nfi
+    desglossament <- data_reactives$desglossament
+    diameter_classes <- data_reactives$diameter_classes
+
+    tables_to_look_at <- c(
+      main_table_to_look_at(nfi, desglossament, diameter_classes),
+      ancillary_tables_to_look_at(nfi)
+    )
+
+    vars_overall <- var_thes %>%
+      dplyr::filter(var_table %in% tables_to_look_at) %>%
+      dplyr::pull(var_id) %>%
+      unique()
+
+    clim_vars <- vars_overall[
+      stringr::str_detect(vars_overall, "^clim_")
+    ]
+
+    plot_vars <- vars_overall[
+      stringr::str_detect(vars_overall, "^admin_|^feat_|^topo_")
+    ]
+
+    removed_vars <- vars_overall[
+      stringr::str_detect(vars_overall, "^old_|^coords_|^presence_|plot_id")
+    ]
+
+    res_vars <- vars_overall[
+      !(vars_overall %in% c(clim_vars, plot_vars, removed_vars))
+    ]
+
+    return(list(
+      res_vars = translate_var(
+        res_vars, tables_to_look_at, lang, var_thes, numerical_thes
+      ),
+      clim_vars = translate_var(
+        clim_vars, tables_to_look_at, lang, var_thes, numerical_thes
+      ),
+      plot_vars = translate_var(
+        plot_vars, tables_to_look_at, lang, var_thes, numerical_thes
+      )
+    ))
+  })
+
   # renderUI ####
   output$mod_filters_container <- shiny::renderUI({
     # ns
     ns <- session$ns
+
+    fil_res_vars_choices <- variables_available()$res_vars
+    fil_clim_vars_choices <- variables_available()$clim_vars
+    fil_plot_vars_choices <- variables_available()$plot_vars
 
     # tagList
     shiny::tagList(
@@ -53,14 +104,20 @@ mod_filters <- function(
           shinyWidgets::pickerInput(
             ns('fil_res_vars'),
             text_translate('fil_res_vars_input', lang, texts_thes),
-            choices = '',
+            choices = fil_res_vars_choices,
             multiple = TRUE,
             options = list(
               `actions-box` = FALSE,
-              `deselect-all-text` = text_translate('deselect-all-text', lang, texts_thes),
-              `select-all-text` = text_translate('select-all-text', lang, texts_thes),
+              `deselect-all-text` = text_translate(
+                'deselect-all-text', lang, texts_thes
+              ),
+              `select-all-text` = text_translate(
+                'select-all-text', lang, texts_thes
+              ),
               `selected-text-format` = 'count > 3',
-              `count-selected-text` = text_translate('count-selected-text-var', lang, texts_thes),
+              `count-selected-text` = text_translate(
+                'count-selected-text-var', lang, texts_thes
+              ),
               `size` = 10,
               `live-search` = TRUE,
               `tick-icon` = 'glyphicon-tree-deciduous'
@@ -72,14 +129,20 @@ mod_filters <- function(
           shinyWidgets::pickerInput(
             ns('fil_clim_vars'),
             text_translate('fil_clim_vars_input', lang, texts_thes),
-            choices = '',
+            choices = fil_clim_vars_choices,
             multiple = TRUE,
             options = list(
               `actions-box` = FALSE,
-              `deselect-all-text` = text_translate('deselect-all-text', lang, texts_thes),
-              `select-all-text` = text_translate('select-all-text', lang, texts_thes),
+              `deselect-all-text` = text_translate(
+                'deselect-all-text', lang, texts_thes
+              ),
+              `select-all-text` = text_translate(
+                'select-all-text', lang, texts_thes
+              ),
               `selected-text-format` = 'count > 3',
-              `count-selected-text` = text_translate('count-selected-text-var', lang, texts_thes),
+              `count-selected-text` = text_translate(
+                'count-selected-text-var', lang, texts_thes
+              ),
               `size` = 10,
               `live-search` = TRUE,
               `tick-icon` = 'glyphicon-tree-deciduous'
@@ -91,14 +154,20 @@ mod_filters <- function(
           shinyWidgets::pickerInput(
             ns('fil_plot_vars'),
             text_translate('fil_plot_vars_input', lang, texts_thes),
-            choices = '',
+            choices = fil_plot_vars_choices,
             multiple = TRUE,
             options = list(
               `actions-box` = FALSE,
-              `deselect-all-text` = text_translate('deselect-all-text', lang, texts_thes),
-              `select-all-text` = text_translate('select-all-text', lang, texts_thes),
+              `deselect-all-text` = text_translate(
+                'deselect-all-text', lang, texts_thes
+              ),
+              `select-all-text` = text_translate(
+                'select-all-text', lang, texts_thes
+              ),
               `selected-text-format` = 'count > 3',
-              `count-selected-text` = text_translate('count-selected-text-var', lang, texts_thes),
+              `count-selected-text` = text_translate(
+                'count-selected-text-var', lang, texts_thes
+              ),
               `size` = 10,
               `live-search` = TRUE,
               `tick-icon` = 'glyphicon-tree-deciduous'
